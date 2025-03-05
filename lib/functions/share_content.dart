5537341c16467bf3993e906541f01725bb08bc96
Parsalp <parsa.osunlou1998@gmail.com>
2025-03-05 15:42:14 +0330

    added screenshot sharing to movies

--- lib/functions/share_content.dart
+++ lib/functions/share_content.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:screenshot/screenshot.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class ShareContent {
   static void shareMovie(int movieId) {
@@ -9,5 +13,45 @@ class ShareContent {
   static void shareTVShow(int serieId) {
     final url = 'https://www.themoviedb.org/tv/$serieId';
     Share.share(url);
+  }
+
+  static Future<void> shareScreenshot(
+      ScreenshotController screenshotController) async {
+    final directory = await getTemporaryDirectory();
+    final imagePath = '${directory.path}/screenshot.png';
+
+    final capturedImage = await screenshotController.capture();
+    if (capturedImage != null) {
+      final imageFile = File(imagePath);
+      await imageFile.writeAsBytes(capturedImage);
+      await Share.shareXFiles([XFile(imagePath)],
+          text: 'Check out this movie!');
+    }
+  }
+
+  static Future<void> sharePartialScreenshot(
+      ScreenshotController screenshotController,
+      Widget widget,
+      int movieId) async {
+    final directory = await getTemporaryDirectory();
+    final imagePath = '${directory.path}/screenshot.png';
+
+    final capturedImage = await screenshotController.captureFromWidget(
+      Material(
+        color: Colors.black,
+        child: widget,
+      ),
+      context: null,
+      delay: const Duration(milliseconds: 100),
+    );
+    final url = 'https://www.themoviedb.org/movie/$movieId';
+
+    if (capturedImage != null) {
+      final imageFile = File(imagePath);
+
+      await imageFile.writeAsBytes(capturedImage);
+      await Share.shareXFiles([XFile(imagePath)],
+          text: '$url\nCheck out this movie!');
+    }
   }
 }
--- lib/moviesPage/movieDetailPage.dart
+++ lib/moviesPage/movieDetailPage.dart
@@ -27,6 +27,7 @@ import 'package:Mirarr/widgets/custom_divider.dart';
 import 'package:Mirarr/widgets/image_gallery_page.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:provider/provider.dart';
+import 'package:screenshot/screenshot.dart';
 
 class MovieDetailPage extends StatefulWidget {
   final String movieTitle;
@@ -47,6 +48,7 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
   dynamic isMovieRated;
   double? userRating;
   double? userScore;
+  final screenshotController = ScreenshotController();
 
   final apiKey = dotenv.env['TMDB_API_KEY'];
 
@@ -339,7 +341,77 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
                           right: 30,
                           child: GestureDetector(
                             onTap: () {
-                              ShareContent.shareMovie(widget.movieId);
+                              showGeneralDialog(
+                                context: context,
+                                barrierDismissible: true,
+                                barrierLabel: '',
+                                transitionDuration:
+                                    const Duration(milliseconds: 300),
+                                pageBuilder:
+                                    (context, animation1, animation2) =>
+                                        Container(),
+                                transitionBuilder:
+                                    (context, animation1, animation2, child) {
+                                  final curvedValue = Curves.easeInOut
+                                          .transform(animation1.value) -
+                                      1.0;
+                                  return Transform(
+                                    transform: Matrix4.translationValues(
+                                        curvedValue * 300, 0, 0),
+                                    child: Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Container(
+                                        height: 200,
+                                        width: 60,
+                                        decoration: BoxDecoration(
+                                          color: Theme.of(context)
+                                              .scaffoldBackgroundColor,
+                                          borderRadius: const BorderRadius.only(
+                                            topLeft: Radius.circular(20),
+                                            bottomLeft: Radius.circular(20),
+                                          ),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 20),
+                                          child: Column(
+                                            mainAxisSize: MainAxisSize.min,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              IconButton(
+                                                onPressed: () {
+                                                  ShareContent.shareMovie(
+                                                      widget.movieId);
+                                                },
+                                                icon: const Icon(
+                                                  Icons.share,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                              const SizedBox(height: 20),
+                                              IconButton(
+                                                onPressed: () {
+                                                  ShareContent
+                                                      .sharePartialScreenshot(
+                                                    screenshotController,
+                                                    _buildScreenShotImage(),
+                                                    widget.movieId,
+                                                  );
+                                                },
+                                                icon: const Icon(
+                                                  Icons.image,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
                             },
                             child: const Icon(
                               Icons.share,
@@ -1191,6 +1263,235 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
               ),
             ),
       bottomNavigationBar: BottomBar(),
+    );
+  }
+
+  Widget _buildScreenShotImage() {
+    final region =
+        Provider.of<RegionProvider>(context, listen: false).currentRegion;
+
+    int? hours = duration != null ? duration! ~/ 60 : null;
+    int? minutes = duration != null ? duration! % 60 : null;
+    String year = releaseDate != null && releaseDate!.isNotEmpty
+        ? releaseDate!.substring(0, 4)
+        : 'NA';
+
+    return Container(
+      constraints: const BoxConstraints(maxHeight: 800),
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Theme.of(context).primaryColor, width: 2),
+        color: Colors.black,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Stack(children: [
+            CachedNetworkImage(
+              imageUrl: '${getImageBaseUrl(region)}/t/p/original$backdrops',
+              placeholder: (context, url) =>
+                  const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+              imageBuilder: (context, imageProvider) => Container(
+                height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: imageProvider,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 320,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [Colors.black, Colors.transparent],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 28,
+              left: 10,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: Text(
+                      widget.movieTitle,
+                      softWrap: true,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: (genres as List<dynamic>).map<Widget>((genre) {
+                return Text(
+                  genre['name'] + ' | ',
+                  softWrap: true,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                      fontFamily: 'RobotoMono'),
+                );
+              }).toList(),
+            ),
+          ),
+          const CustomDivider(),
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              about!,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+                fontWeight: FontWeight.w200,
+                fontFamily: 'Poppins',
+              ),
+              textAlign: TextAlign.left,
+              maxLines: 8,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          const CustomDivider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: 110,
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  decoration: BoxDecoration(
+                    color: getMovieBackgroundColor(context, widget.movieId),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Duration',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w200,
+                          fontFamily: 'RobotoMono',
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "${hours}H ${minutes}M",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 110,
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  decoration: BoxDecoration(
+                    color: getMovieBackgroundColor(context, widget.movieId),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Year',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w200,
+                          fontFamily: 'RobotoMono',
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          year,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 110,
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  decoration: BoxDecoration(
+                    color: getMovieBackgroundColor(context, widget.movieId),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Language',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w200,
+                          fontFamily: 'RobotoMono',
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          language != null ? language!.toUpperCase() : 'N/A',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
