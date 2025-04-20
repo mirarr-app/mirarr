c512c9f2050cd1faf52a636dad10d4e1dfb3add5
Parsalp <parsa.osunlou1998@gmail.com>
2025-04-20 17:51:52 +0330

    play with vlc series

--- lib/moviesPage/functions/check_xprime.dart
+++ lib/moviesPage/functions/check_xprime.dart
@@ -20,3 +20,24 @@ Future<bool> checkXprime(int movieId) async {
     return false;
   }
 }
+
+
+Future<bool> checkXprimeSeries(int serieId, int seasonNumber, int episodeNumber) async {
+  try {
+    final response = await http.get(Uri.parse(
+        'https://xprime.tv/primebox?id=$serieId&season=$seasonNumber&episode=$episodeNumber'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data['status'] == 'ok') {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+}
