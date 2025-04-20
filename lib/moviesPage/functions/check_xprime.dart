47d2f252691dc7d504ab5bd9f1b87342267cc60b
Parsalp <parsa.osunlou1998@gmail.com>
2025-04-20 17:12:48 +0330

    play with vlc

---
+++ lib/moviesPage/functions/check_xprime.dart
@@ -0,0 +1,22 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<bool> checkXprime(int movieId) async {
+  try {
+    final response = await http.get(Uri.parse(
+        'https://xprime.tv/primebox?id=$movieId'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data['status'] == 'ok') {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+}
